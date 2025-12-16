5b328cd7d456f738bf38e28cb40a3b7547426026
saanvi-cloud <saanvidd2003@gmail.com>
2025-12-15 23:07:57 -0800

    Completed dashboard and student sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,12 @@ import { Component, signal } from '@angular/core';
 import { Header } from './components/header/header';
 import { Navbar } from './components/navbar/navbar';
 import { Dashboard } from './components/dashboard/dashboard';
+import { Students } from './components/students/students';
+import { AddStudent } from './components/add-student/add-student';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Navbar, Dashboard],
+  imports: [Header, Navbar, Dashboard, Students, AddStudent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
